54286e274fb87aa3578b52772f37955f66022052
Younes_Programer <programationy@gmail.com>
2024-12-10 16:27:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>YounesProjectTesting</title>
+    </head>
+    <body>
+        <h1>SUBMIT FORME</h1>
+
+
+        <form method="post" action="form.php">
+
+
+        <label for="name"> name:</label><br>
+        <input type="text" id="name" name="name" required><br>
+        <label for="email"> email</label><br>
+        <input type="email" id="email" name="email" required><br>
+        <label for="phonenumber"> phonenumber</label><br>
+        <input type="text" id="phonenumber" name="phonenumber" required><br>
+
+
+
+        <input type="submit" value="Submit">
+
+
+
+
+
+
+        </form>
+
+
+
+
+    </body>
+</html>
